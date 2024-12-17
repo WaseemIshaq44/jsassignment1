8a1913e36d0440a7711adb37148a3424c7c18a0a
WaseemIshaq44 <waseemishaq44@gmail.com>
2024-12-17 18:24:25 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// Chapter 1
+//Question 1
+// alert("Welcome to Our Website!");
+
+//Question 2
+// alert("Error! Please enter a valid email address");
+
+//Question 3
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+//Question 4
+// alert("Welcome to JS Land...");
+// alert("Prevent this page from creating additional dialogs.");
+
+//Question 5
+// alert("Hello I can run JS through my web browser's console");
+
+
+//Chapter 2
+//Question 1
+// var userName;
+
+//Question 2
+// var myName = "Waseem Ishaq";
+
+//Question 3
+// var message;
+// message = "Hello, world!";
+// alert(message);
+
+//Question 4
+// var name="Jhone Doe";
+// alert(name);
+// var age="15 years old";
+// alert(age);
+// var profession="Certified Mobile Application Development";
+// alert(profession);
+
+//Question 5
+// var a="PIZZA";
+// alert(a+ "\n" + a[0]+a[1]+a[2]+a[3] + "\n" + a[0]+a[1]+a[2] + "\n" + a[0]+a[1] + "\n" + a[0]);
+
+//Question 6
+// var email;
+// email="waseemishaq44@gmail.com";
+// alert("my email address is " + email)
+
+//Question 7
+// var book;
+// book="A smarter way to learn JavaScript"
+// alert(book);
+
+// Question 8
+// document.write("<p>Yah! I can write HTML content through JavaScript</p>");
+
+//Question 9
+// var x="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(x);
+
+
+//Chapter 3
+//Question 1
+// var age ="23";
+// alert("I am "+age+" years old");
+
+//Question 2
+// var N=14;
+// alert("you have visited this site "+ N +" times");
+
+//Question 3
+// let birthYear;
+// birthYear = 2001;
+// document.write("<p>"+"My birth year is "+ birthYear+ "<br/> Data type of my declaired variable is number" +"</p>");
+
+//Question 4
+// let company="XYZ Clothing store"
+// let visitorName;
+// let product;
+// let quantity;
+// visitorName="John Doe";
+// product="T-Shirt";
+// quantity="5";
+// document.write("<p>"+ " "+"<b>"+visitorName+"</b>"  + " " +"ordered"+ " " +"<b>"+ quantity+" "+ product+"</b>" +"(s) on "+ company+"."+"</p>");
+
+
+
